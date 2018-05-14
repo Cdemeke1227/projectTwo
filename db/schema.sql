4c4d25f3ff67f14cb9607ac824ccdd59bb942d3c
Abdolbaghi Teygh khorshid <34252946+Teygh@users.noreply.github.com>
2018-05-14 18:31:13 -0500

    added dummmy data and added timestamps

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 -- Drops the hairsalon_db if it exists currently --
 DROP DATABASE IF EXISTS hairsalon_db;
 -- Creates the "hairsalon" database --
+=======
+-- Drops the Hairsalon_db if it exists currently --
+DROP DATABASE IF EXISTS hairsalon_db;
+-- Creates the "hairsalon_db" database --
+>>>>>>> Stashed changes
 CREATE DATABASE hairsalon_db;

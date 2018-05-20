d24204f114e26d73af3e887fcc590c52e2047a3a
christopher demeke <christopher.demeke@gmail.com>
2018-05-20 16:03:55 -0500

    schedule add/edit post modal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,74 +1,103 @@
 $(function () {
 
-var offset = 120;
+    var offset = 120;
 
-$('#navPageLinks ul li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
-});
-
-
-
-// SignUp modeal open
-function signupModal () {
-    event.preventDefault();
-    $('#signupModal').modal('toggle');
-    $('#signupModal').modal('show');
-console.log("hello");
-}
+    $('#navPageLinks ul li a').click(function (event) {
+        event.preventDefault();
+        $($(this).attr('href'))[0].scrollIntoView();
+        scrollBy(0, -offset);
+    });
 
 
-// In order launch sign up modal place targer #placeholde
-var signupErr = $("#sign-up-error-msg").text().trim().length;
-var loginErr = $("#login-error-msg").text().trim().length;
-var settingsErr = $("#settings-error-msg").text().trim().length;
 
-console.log(signupErr);
-if(signupErr > 1){
-    $('#signupModal').modal('toggle');
-    $('#signupModal').modal('show');
-};
-if(loginErr > 1){
-    $('#loginModal').modal('toggle');
-    $('#loginModal').modal('show');
-};
-
-if(settingsErr > 1){
-    $("#userSettingsModal").modal('toggle');
-    $("#userSettingsModal").modal('show');
-}
+    // SignUp modeal open
+    function signupModal() {
+        event.preventDefault();
+        $('#signupModal').modal('toggle');
+        $('#signupModal').modal('show');
+        console.log("hello");
+    }
 
 
-// Changes what's being displayed in settingsModal as the user changes the value of the input
-$('.settingsChild').keyup(function(){
+    // In order launch sign up modal place targer #placeholde
+    var signupErr = $("#sign-up-error-msg").text().trim().length;
+    var loginErr = $("#login-error-msg").text().trim().length;
+    var settingsErr = $("#settings-error-msg").text().trim().length;
 
-    $(this).closest('.settingsParent').children('.childText').text($(this).val().trim());
-});
+    console.log(signupErr);
+    if (signupErr > 1) {
+        $('#signupModal').modal('toggle');
+        $('#signupModal').modal('show');
+    };
+    if (loginErr > 1) {
+        $('#loginModal').modal('toggle');
+        $('#loginModal').modal('show');
+    };
+
+    if (settingsErr > 1) {
+        $("#userSettingsModal").modal('toggle');
+        $("#userSettingsModal").modal('show');
+    }
 
 
-var providerQuery = `/api/retrieve/providers/?all=yes`; 
+    // Changes what's being displayed in settingsModal as the user changes the value of the input
+    $('.settingsChild').keyup(function () {
+
+        $(this).closest('.settingsParent').children('.childText').text($(this).val().trim());
+    });
+
+
+    var providerQuery = `/api/retrieve/providers/?all=yes`;
     console.log(providerQuery);
     //Make the ajax call
     $.ajax(providerQuery, {
-      type: "GET",
-      success: function (data) {
+        type: "GET",
+        success: function (data) {
 
-        for (var i = 0; i < data.length; i++) {
-          var providerFirstName = data[i].provider.firstName;
-          var providerID = data[i].provider.id;
-          // console.log('Provider Data: ' + providerFirstName);
+            for (var i = 0; i < data.length; i++) {
+                var providerFirstName = data[i].provider.firstName;
+                var providerID = data[i].provider.id;
+                // console.log('Provider Data: ' + providerFirstName);
+                console.log('Provider Data: ' + providerID);
 
-          var providerList = $("#providerChoice");
-          var optionProvider = $("<option>");
-          optionProvider.text(providerFirstName);
-          optionProvider.val(providerID);
-          providerList.append(optionProvider);
+                var providerList = $("#providerChoice");
+                var optionProvider = $("<option>");
+                optionProvider.text(providerFirstName);
+                optionProvider.val(providerID);
+                providerList.append(optionProvider);
+            }
+        },
+        error: function (request, error) {
+            alert("Request: " + JSON.stringify(request));
         }
-      },
-      error: function (request, error) {
-        alert("Request: " + JSON.stringify(request));
-      }
+    });
+
+
+
+
+    $(".scheduleForm").on("submit", function (e) {
+        e.preventDefault();
+
+        console.log($("providerPick").val());
+
+        var scheduleInput = {
+            startTime: $("#inputStart").val().trim(),
+            endTime: $("#inputEnd").val().trim(),
+            ProviderId: 4
+        }
+        console.log(scheduleInput);
+
+
+        $.ajax("/api/schedules", {
+            type: "POST",
+            data: scheduleInput
+        }).then(
+            function () {
+                console.log(`Inserted new schedule item, Start Time: ${scheduleInput.startTime}, End Time: ${scheduleInput.endTime}, Provider Id: ${scheduleInput.ProviderId} `);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
     });
 
 
